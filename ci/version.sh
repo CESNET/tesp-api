f39caf9a0a6a99dd15dd4326b6e08e230008d50b
Jan Krystof <jan.krystof@cesnet.cz>
2024-09-23 13:08:06 +0200

    #i1 | introducing semantic versioning

---
+++ ci/version.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Get project version
+# Usage:
+# * get-version <changelog-version-file>
+
+CHANGELOG_FILE="$1"
+
+changelog_version=$(grep -E '^##[ \t]+\[.+\]' "${CHANGELOG_FILE}" | \
+                    awk '{print substr($2,2,length($2)-2)}' | grep -v '[Uu]nreleased' | head -1)
+
+echo "${changelog_version}"
