553182c00bebbb990eedc5bc4216df0912fa2488
Boris Jurič <74237898+BorisYourich@users.noreply.github.com>
2025-04-02 16:52:36 +0200

    Update basic_auth.py

--- tesp_api/utils/basic_auth.py
+++ tesp_api/utils/basic_auth.py
@@ -1,9 +1,10 @@
 import secrets
+from tesp_api.config.properties import properties
 from tesp_api.service.error import BasicAuthError
 
 def verify_basic_auth(username: str, password: str):
-    valid_username = "user"
-    valid_password = "password"
+    valid_username = properties.basic_auth.username
+    valid_password = properties.basic_auth.password
     
     if secrets.compare_digest(username, valid_username) and secrets.compare_digest(password, valid_password):
         return username 
